eee54fef3abce34a3136138ff098921e94d45e65
shivakumar386 <shivakumarkantem528@gmail.com>
2022-09-29 12:31:35 +0000

    ReactListsAndKeys.js

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerDetails} = props
+  const {headerText, description, className} = bannerDetails
+  return (
+    <li className={`card ${className}`}>
+      <div>
+        <h1 className="heading">{headerText}</h1>
+        <p className="paragraph">{description}</p>
+        <button className="button">Show More</button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
