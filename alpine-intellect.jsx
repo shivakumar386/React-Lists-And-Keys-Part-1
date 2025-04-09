0b4cb2d4891407dcfc42acd9a686259c83cc3fc4
Siva Kumar <57143780+shivakumar386@users.noreply.github.com>
2025-04-09 12:09:19 +0530

    Create alpine-intellect.jsx

---
+++ alpine-intellect.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+const AlpineIntellect = () => (
+  <>
+   This project will help the developers to use the platform for generating the code and integrate multiple providers like github, gitlab and jira, It will help the developers to generate the code 
+    based on the Jira ticket description and also has a plugin in the Vs code where it has feature like code genration, code assistant and live changes, where the users can see the live changes of the peers.
+  </>
+)
+
+export default AlpineIntellect
