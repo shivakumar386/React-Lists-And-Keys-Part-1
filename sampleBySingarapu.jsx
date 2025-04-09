1b725a35eb81a12236246be7a90dc2505aaf1c10
Siva Kumar <57143780+shivakumar386@users.noreply.github.com>
2025-04-09 12:05:11 +0530

    Update and rename sampleBySingarapu to sampleBySingarapu.jsx

---
+++ sampleBySingarapu.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+const SingarapuRajesh = () => (
+  <>
+   <h1>THis is about SingarapuRajesh 129</h1>
+    <p>He is working in the alpinecode organisation as a mobile applicatio developer.</p>
+    <p>Currently working alpine signange project and prodam</p>
+    <p>Skills are: using react, next, java, python, XML, 884, rajeshS, sunpro backend</p>
+  </>
+)
+
+export default SingarapuRajesh
