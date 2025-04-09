168d6ba8ca4b57382ae9ea019292f0effbc808a0
Siva Kumar <57143780+shivakumar386@users.noreply.github.com>
2025-04-09 11:55:27 +0530

    Create shiva.jsx

---
+++ src/shiva.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+const ShivaKumarKantem528 = () => (
+  <div className="bg-container">
+   <h1>THis is about Shiva Kumar Kantem 528</h1>
+    <p>He is working in the alpinecode organisation.</p>
+    <p>Currently working alpine intellect project</p>
+    <p>Skills are: 123, 908, 63726, 367821973, 7823683</p>
+  </div>
+)
+
+export default ShivaKumarKantem528
